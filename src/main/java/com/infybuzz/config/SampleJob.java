4724609d73ca061a062e88e1b23f8d951d5c44dd
Mohamed Ibrahim <Mohamed Ibrahim@LAPTOP-QN503SBS>
2024-03-05 11:28:31 +0530

    Added second step

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@SuppressWarnings("removal")
+
 @Configuration
 public class SampleJob {
 
@@ -24,17 +24,23 @@ public class SampleJob {
     @Bean
 	public Job firstJob() {
     	
-    	 return jobBuilderFactory.get("First Job").start(firstStep()).build();
+    	 return jobBuilderFactory.get("First Job")
+    			 .start(firstStep())
+    			 .next(secondStep())
+    			 .build();
 	 
    }
     
    public Step firstStep() {
 	   
-	   return stepBuilderFactory.get("First Step").tasklet(firstTask()).build();
+	   return stepBuilderFactory.get("First Step")
+			   .tasklet(firstTask())
+			   .build();
 	   
 	 
    }
    
+
    public Tasklet firstTask() {
 			   
 			   
@@ -42,13 +48,40 @@ public class SampleJob {
 		
 		@Override
 		public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-			// TODO Auto-generated method stub
+
+
 			
-			 System.out.println("This is the first job");
+			 System.out.println("This is the first tasklet step");
 			return RepeatStatus.FINISHED; 
 		}
 	};
    }
+   
+   
+public Step secondStep() {
+	   
+	   return stepBuilderFactory.get("Second Step")
+			   .tasklet(secondTask())
+			   .build();
+	   
+	 
+   }
+
+   public Tasklet secondTask() {
+	   
+	   
+		 return  new Tasklet() {
+	
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+
+		
+		 System.out.println("This is the second tasklet step");
+		return RepeatStatus.FINISHED; 
+	}
+};
+}
     
     
     
